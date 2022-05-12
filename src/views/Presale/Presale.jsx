e9244a41d418c6988da26e82cb7c2e2b39718e80
neojibaros <jibarodao@protonmail.com>
2022-05-12 09:09:47 +0000

    cambiar contrato y nombre del artdrop

--- src/views/Presale/Presale.jsx
+++ src/views/Presale/Presale.jsx
@@ -67,13 +67,13 @@ function Presale() {
         <Grid container direction="column" spacing={2}>
           <Grid item>
             <div className="card-header">
-              <Typography variant="h5">¡Llegó el Artdrop de la Mujer!</Typography>
+              <Typography variant="h5">¡Llegó el Artdrop de la Naturaleza!</Typography>
               <p> {address} </p>
             </div>
           </Grid>
           <Grid item>
             <div className="stake-top-metrics">
-              <Typography className="presale-items">Para redimir tus 12 NFTs en honor a la mujer, presiona:</Typography>
+              <Typography className="presale-items">Para redimir tus 12 NFTs en honor a nuestra Madre Naturaleza, presiona:</Typography>
               <div style={{justifyContent: "center", display: "flex"}}>
                 <Button
                           className="stake-button"
